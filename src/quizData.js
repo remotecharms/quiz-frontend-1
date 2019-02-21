b00a13404e34fb21072c6108deddcbe26a5d18a7
Ruth Nassar <nassar_ruth@yahoo.co.uk>
2019-02-21 10:04:47 +0000

    added file for question data and sample questions

---
+++ src/quizData.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        question: 'In JavaScript, which of these is known as an assignment operator?',
+        answers: ['*', '-', '=', '+'],
+        correct: 3
+    },
+
+    {
+        question: 'In Javascript, which of these is these is the logical operator meaning, "both these things must be true?',
+        answers: ['&&', '||', '!', '++'],
+        correct: 1
+    },    
+    {
+        question: 'Which git command allows you to view your previous commits?',
+        answers: ['git status', 'git checkout', 'git reset', 'git log'],
+        correct: 4
+    }   
+]

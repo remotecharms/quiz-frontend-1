d6d02dd2132a3b6650037f2693cc108133ab4159
Ruth Nassar <nassar_ruth@yahoo.co.uk>
2019-02-22 11:08:58 +0000

    introduced styles to quiz page, added button margin

--- src/components/quiz.js
+++ src/components/quiz.js
@@ -9,22 +9,20 @@ class Quiz extends React.Component {
             <div className = "row">
             <p>Question 1 some text goes here</p>
             </div>
-
-
             <div className = "row">
-            <button type = "button">Answer 1</button>
+            <button type = "button" style = {styles.button}>Answer 1</button>
             </div>
             
             <div className = "row">
-            <button type = "button">Answer 2 </button>
+            <button type = "button" style = {styles.button}>Answer 2 </button>
             </div>
 
             <div className = "row">
-            <button type = "button">Answer 3 </button>
+            <button type = "button" style = {styles.button} >Answer 3 </button>
             </div>
 
             <div className = "row">
-            <button type = "button">Answer 4</button>
+            <button type = "button" style = {styles.button} >Answer 4</button>
             </div>
 
         </div>
@@ -32,4 +30,10 @@ class Quiz extends React.Component {
     }
 }
 
+const styles = {
+    button: {
+       margin: "10px"
+    }
+
+};
 export default Quiz;

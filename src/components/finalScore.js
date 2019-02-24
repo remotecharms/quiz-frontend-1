d93fd6dcfe3edea3fe3f1e0e7d0e76ed853e6c3c
remotecharms <emma_crothers@hotmail.com>
2019-02-24 16:36:41 +0000

    finalScore.js file added and hardcoded

---
+++ src/components/finalScore.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class finalScore extends React.Component {
+
+    render() {
+        return (
+            <div className="row">
+                Oh dear! Your final score is: 0
+                </div>
+        )
+    }
+};
+
+
+export default finalScore; 
